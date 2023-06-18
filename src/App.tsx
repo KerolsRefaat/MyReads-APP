ca153f733a80bfe2f48d30af77b2d5ed698fc5fc
Kerols Refaat <kerolsrefaat3@gmail.com>
2023-06-18 17:25:42 +0300

    Trying Lazy from React

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, {lazy} from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
-import SearchPage from "./Components/SearchPage";
 import Header from "./Components/Header";
 import BookList from "./Components/BookList";
 import Button from "./Components/Button";
+const SearchPageComponent = lazy(() => import("./Components/SearchPage"));
 
 
 const App: React.FC = () => {
@@ -23,7 +23,7 @@ const App: React.FC = () => {
           }
         ></Route>
        
-        <Route path="/search" element={<SearchPage />}></Route>
+        <Route path="/search" element={<SearchPageComponent/>}></Route>
       </Routes>
     </BrowserRouter>
   );
